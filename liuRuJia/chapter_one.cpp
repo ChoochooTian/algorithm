25d9da156e98b15267a308aa757e9cf478d596e6
田松岩 <songyantian@outlook.com>
2018-04-01 18:57:55 +0800

    Update chapter_one.cpp

--- liuRuJia/chapter_one.cpp
+++ liuRuJia/chapter_one.cpp
@@ -1,4 +1,28 @@
-//1-6
+//例题1-1 圆柱体的表面积
+/*
+输入底面半径r和高h，输出圆柱体的表面积，保留三位小数。
+样例输入：
+3.5 9
+样例输出：
+Area = 274.889 
+*/ 
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	const double pi = acos(-1.0);  //const 定义常量
+	double r,h,s1,s2,s;
+	scanf("%lf%lf",&r,&h);  //double输入 lf
+	s1=pi*r*r;
+	s2=2*pi*r*h;
+	s=2.0*s1+s2;
+	printf("Area = %.3f\n",s);
+	return 0;
+}
+
+//反余弦函数表示PI 　acos(-1.0) = π
+// 如果实在不理解就画一下三角函数图像 
 
 //1-7
 #include<stdio.h>
